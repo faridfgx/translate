1e90e57e7337b65aae0ccb260046ce86cfd65413
faridfgx <faridfgx200@gmail.com>
2025-02-28 14:32:27 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    API_KEY: '',
+    API_KEY: 'AIzaSyAd6gZCsIIcAZ1hDbYJ--XVqn8_8z88Oms',
     TTS_API_URL: 'https://texttospeech.googleapis.com/v1/text:synthesize',
     TRANSLATE_API_URL: 'https://translation.googleapis.com/language/translate/v2',
     DETECT_API_URL: 'https://translation.googleapis.com/language/translate/v2/detect'
